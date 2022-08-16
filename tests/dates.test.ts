3cae19aedad56ccfd6a7f50b39f3a68464897b7a
g4rcez <allan.f.garcez@gmail.com>
2022-08-15 23:00:47 -0300

    wip

--- tests/dates.test.ts
+++ tests/dates.test.ts
@@ -2,11 +2,6 @@ import { describe, expect, test } from "vitest";
 import { Dates } from "../src/lib/dates";
 
 describe("Dates", () => {
-    test("nextDay", () => {
-        const date = new Date("1970-01-01T00:00:00.000Z");
-        expect(Dates.nextDay(date).toISOString()).toBe("1970-01-02T02:59:59.999Z");
-    });
-
     test("epoch - zero time", () => {
         const date = new Date("1970-01-01T00:00:00.000Z");
         expect(Dates.epoch(date)).toBe(0);
